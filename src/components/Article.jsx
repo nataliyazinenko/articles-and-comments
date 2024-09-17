124824b90502ce920fb2ab2204b85ad64b0632fe
nataliyazinenko <n.zinenko@gmail.com>
2024-09-17 22:56:56 +0100

    wip - handle errors

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -39,20 +39,32 @@ const Article = () => {
   const handleVotePlus = () => {
     if (myVote === 1) {
       setVotes((oldValue) => oldValue - 1);
-      patchArticle(article_id, -1);
       setMyVote(null);
-      localStorage.removeItem(`votes_${article_id}`);
+      patchArticle(article_id, -1)
+        .then((res) => {
+          setMyVote(null);
+          localStorage.removeItem(`votes_${article_id}`);
+        })
+        .catch((err) => {
+          setMyVote(1);
+          setVotes((oldValue) => oldValue + 1);
+        });
     } else if (myVote === -1) {
       setVotes((oldValue) => oldValue + 2);
       patchArticle(article_id, 2);
       setMyVote(1);
       localStorage.setItem(`votes_${article_id}`, 1);
-    } else if (myVote === null) {
+    }
+
+    // setMyVote(-1);
+    else if (myVote === null) {
       setVotes((oldValue) => oldValue + 1);
       patchArticle(article_id, 1);
       setMyVote(1);
       localStorage.setItem(`votes_${article_id}`, 1);
     }
+
+    // setMyVote(null)
   };
 
   const handleVoteMinus = () => {
